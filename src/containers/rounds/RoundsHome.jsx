0f6c18dd5927a51b4968fafec4571f72bbaee3d0
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-09-01 21:02:54 +0530

    implement rounds home page

--- src/containers/rounds/RoundsHome.jsx
+++ src/containers/rounds/RoundsHome.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const RoundsHome = () => {
   return (
     <div>
-      <h1>Rounds Home Page</h1>
+      <h1 className="font-weight-light mb-3">QuadDAO Rounds</h1>
+      <h4 className="font-weight-light">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam modi,
+        optio amet deserunt ratione magni! Repudiandae provident nobis velit quo
+        ipsam, ut at quidem omnis. Dignissimos culpa quae commodi ad?
+      </h4>
+      <button className="btn btn-success mr-3">Enter Round</button>
+      <button className="btn btn-primary">Know More</button>
+      <hr />
+      <h4 className="text-center mt-5">
+        Funding Round 5 is here! We have 15 sponsors bringing in $25000 to the
+        match pool. <Link to="/rounds/contribute">Contribute</Link> now to your
+        favourite projects
+      </h4>
     </div>
   );
 };
