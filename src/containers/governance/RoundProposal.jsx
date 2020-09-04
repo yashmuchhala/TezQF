b8464d9ef61afbe1cf3a274c1413a83b9892f988
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-09-04 22:15:57 +0530

    integrate ipfs and add validations for new round proposal

--- src/containers/governance/RoundProposal.jsx
+++ src/containers/governance/RoundProposal.jsx
@@ -1,10 +1,46 @@
 import React, { useState } from "react";
 
+import { proposalVaidations as validations } from "../../utils/validations";
+
+const ipfs = require("nano-ipfs-store").at("https://ipfs.infura.io:5001");
+
 const RoundProposal = () => {
   const [start, setStart] = useState(new Date());
   const [end, setEnd] = useState(new Date());
   const [categories, setCategories] = useState("");
   const [description, setDescription] = useState("");
+  const [datesError, setDatesError] = useState(false);
+  const [categoriesError, setCategoriesError] = useState(false);
+  const [descriptionError, setDescriptionError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!start || !end || start < new Date() || end <= start) {
+      setDatesError(true);
+      return;
+    }
+    if (!categories) {
+      setCategoriesError(true);
+      return;
+    }
+    if (!validations.description(description)) {
+      setDescriptionError(true);
+      return;
+    }
+
+    setDatesError(false);
+    setCategoriesError(false);
+    setDescriptionError(false);
+
+    const ipfsObject = {
+      description,
+      categories: categories.split(","),
+    };
+
+    const cid = await ipfs.add(JSON.stringify(ipfsObject));
+    console.log(cid);
+  };
 
   return (
     <div className="container w-75 mb-5">
@@ -15,23 +51,34 @@ const RoundProposal = () => {
         transaction.
       </p>
       <br />
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row no-gutters">
           <div className="col mr-2">
-            <label className="font-weight-bold mb-0">Start Date</label>
+            <label className="font-weight-bold mb-0">
+              Start Date<sup className="text-danger">*</sup>
+            </label>
             <input
               type="date"
-              className="form-control mb-3"
+              className={`form-control ${
+                datesError ? "border-danger mb-0" : "mb-3"
+              }`}
               placeholder="Start Date"
               value={start}
               onChange={(e) => setStart(e.target.value)}
             />
+            {datesError ? (
+              <div className={`text-danger mb-3`}>Please enter valid dates</div>
+            ) : null}
           </div>
           <div className="col ml-2">
-            <label className="font-weight-bold mb-0">End Date</label>
+            <label className="font-weight-bold mb-0">
+              End Date<sup className="text-danger">*</sup>
+            </label>
             <input
               type="date"
-              className="form-control mb-3"
+              className={`form-control ${
+                datesError ? "border-danger mb-0" : "mb-3"
+              }`}
               placeholder="End Date"
               value={end}
               onChange={(e) => setEnd(e.target.value)}
@@ -39,24 +86,42 @@ const RoundProposal = () => {
           </div>
         </div>
 
-        <label className="font-weight-bold mb-0">Entry Categories</label>
+        <label className="font-weight-bold mb-0">
+          Entry Categories<sup className="text-danger">*</sup>
+        </label>
         <input
           type="text"
-          className="form-control mb-3"
-          placeholder="Categories"
+          className={`form-control ${
+            categoriesError ? "border-danger mb-0" : "mb-3"
+          }`}
+          placeholder="Enter comma separated categories"
           value={categories}
           onChange={(e) => setCategories(e.target.value)}
         />
+        {categoriesError ? (
+          <div className={`text-danger mb-3`}>
+            Please enter at least one category
+          </div>
+        ) : null}
 
-        <label className="font-weight-bold mb-0">Description</label>
+        <label className="font-weight-bold mb-0">
+          Description<sup className="text-danger">*</sup>
+        </label>
         <textarea
           type="text"
-          className="form-control mb-3"
+          className={`form-control ${
+            descriptionError ? "border-danger mb-0" : "mb-3"
+          }`}
           placeholder="Enter a short description for the proposed funding round..."
           rows={10}
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
+        {descriptionError ? (
+          <div className={`text-danger mb-3`}>
+            Please enter 1-500 characters
+          </div>
+        ) : null}
 
         <button className="btn btn-lg btn-outline-primary font-weight-bold">
           Confirm Proposal
--- src/utils/validations.js
+++ src/utils/validations.js
@@ -2,18 +2,22 @@
 const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
 
 const validUrl = (url) => urlRegex.test(url);
-const validProjectTitle = (title) => title.length > 0 && title.length <= 50;
-const validProjectDescription = (description) =>
+const validTitle = (title) => title.length > 0 && title.length <= 50;
+const validDescription = (description) =>
   description.length > 0 && description.length <= 500;
 const validTezosAddress = (address) => address.length === 36;
 
 export const projectEntryValidations = {
   image: validUrl,
-  title: validProjectTitle,
-  description: validProjectDescription,
+  title: validTitle,
+  description: validDescription,
   website: validUrl,
   twitter: validUrl,
   github: validUrl,
-  category: validProjectTitle,
+  category: validTitle,
   address: validTezosAddress,
+};
+
+export const proposalVaidations = {
+  description: validDescription,
 };
