df70bcc1d8eb820faa202265b0144389d05154c3
Guna Shekar <gunashekherproddatoori@gmail.com>
2020-09-27 14:36:04 +0530

    fix card alignment issues

--- src/components/rounds/ArchiveCard.jsx
+++ src/components/rounds/ArchiveCard.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const ArchiveCard = ({ details }) => {
   console.log(details);
   return (
-    <div className="col-4 mb-4">
+    <div className="d-flex col-4 mb-4">
       <div className="card">
         <img
           className="card-img-top"
           src={details.image}
           alt="Project Background"
         />
-        <div className="card-body">
+        <div className="card-body d-flex flex-column">
           <h5 className="card-title">{details.title}</h5>
           <p className="card-text">
             {details.description.length > 80
@@ -43,7 +43,7 @@ const ArchiveCard = ({ details }) => {
               </div>
             </div>
           )}
-          <Link to={`/archives/${details.id}`}>
+          <Link to={`/archives/${details.id}`} className="mt-auto">
             <button className="btn btn-primary btn-block">View</button>
           </Link>
         </div>
--- src/components/rounds/ProjectCard.jsx
+++ src/components/rounds/ProjectCard.jsx
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 
 const ProjectCard = ({ details }) => {
   return (
-    <div className="col-4 mb-4">
-      <div className="card">
+    <div className="d-flex col-4 mb-4">
+      <div className="card align-self-stretch">
         <img
           className="card-img-top"
           src={details.image}
           alt="Project Background"
         />
-        <div className="card-body">
+        <div className="card-body d-flex flex-column">
           <h5 className="card-title">{details.title}</h5>
           <p className="card-text">
             {details.description.length > 80
               ? details.description.slice(0, 80) + "..."
               : details.description}
           </p>
-          <Link to={`/contribute/${details.id}`}>
+          <Link to={`/contribute/${details.id}`} className="mt-auto">
             <button className="btn btn-primary btn-block">Contribute</button>
           </Link>
         </div>
